5fbb70f1bbcdf419051e63fb95b5b06c3baa6822
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-04-29 15:32:00 -0300

    Add files via upload

---
+++ artASCII/art2.bat
@@ -0,0 +1,13 @@
+echo                  +---------------------------+
+echo                  ^|   1: Backup -^> Desktop    ^|
+echo                  ^|   2: Backup -^> Imagens    ^|
+echo                  ^|   3: Backup -^> Videos     ^|
+echo                  ^|   4: Backup -^> Documentos ^|
+echo                  ^|   5: Backup -^> Musicas    ^|
+echo                  ^|   6: Backup -^> Downloads  ^|
+echo                  ^|---------------------------^|
+echo                  ^|---------------------------^|
+echo                  ^|   7: Trocar unidade       ^|
+echo                  ^|---------------------------^|
+echo                  ^|   8: Sair                 ^| 
+echo                  +---------------------------+
